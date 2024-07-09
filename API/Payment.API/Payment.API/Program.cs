f60778cfa97249d0cfdb3d3e670c920ede6978a9
Ryan Clarian <rclarian@gmail.com>
2024-07-09 22:04:40 +0800

    Added PaymentDetailContext, connectionstring
    Added PaymentDetailContext, connectionstring

--- API/Payment.API/Payment.API/Program.cs
+++ API/Payment.API/Payment.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Payment.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//DB injection 
+builder.Services.AddDbContext<PaymentDetailContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("PaymentDetailConnection"));
+});
+
 
 var app = builder.Build();
 
